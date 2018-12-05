0fabb70395fa8543c8e9ecfb2badf13e8ac09fb5
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2018-12-05 15:56:50 +0600

    page view

--- packages/dashboard/urls.py
+++ packages/dashboard/urls.py
@@ -6,4 +6,5 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.index, name='dashboard'),
     path('create/', views.create_page, name='create-new-page'),
+    path('<page_id>/view/', views.view_page, name='view-page'),
 ]
