ce03131ef6e0210efe0c1eb6fd04c7eacd75cb15
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2018-12-06 18:33:19 +0600

    all blog page working

--- packages/writing/views/blog.py
+++ packages/writing/views/blog.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from writing.models import Post
 
 
 def all_blogs(request):
-    return HttpResponse("all blog page")
+    blogs = Post.objects.filter(post_type='b', status='p').order_by('-created_time')
+    # print("all blogs: {}".format(blogs))
+    ctx = {
+        "blogs": blogs,
+    }
+    return render(request, 'post/blog/all-blogs.html', ctx)
