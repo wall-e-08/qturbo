890f4cebfc51b9c86c83f5105e447ac223220c5f
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2018-12-11 15:24:54 +0600

    dashboard page updated, bootstrap-vue code updated

--- packages/dashboard/views.py
+++ packages/dashboard/views.py
@@ -2,16 +2,36 @@ from django.shortcuts import render, redirect
 from django.http import Http404, HttpResponse
 from .forms import PageForm, BlogForm
 from .models import Page
+from writing.models import Article, Blog, Category
+
+"""login_required decorator added in urls.py... So no need to add here"""
 
 
 def index(request):
-    return render(request, 'dashboard/index.html')
-
+    data = {
+        "article": {
+            "count": Article.objects.all().count(),
+            "section_count": len(set(Article.objects.exclude(section=None).values_list('section', flat=True)))
+        },
+        "blog": {
+            "count": Blog.objects.all().count(),
+            "category_count": Category.objects.all().count(),  # category used only in blog, no need to filter
+            "section_count": len(set(Blog.objects.exclude(section=None).values_list('section', flat=True)))
+        },
+    }
+    return render(request, 'dashboard/index.html', {"data": data})
 
 def all_pages(request):
     all_page = Page.objects.all()
     return render(request, 'dashboard/all_pages.html', {
         "pages": all_page,
+    })
+
+
+def all_blogs(request):
+    blogs = Blog.objects.all()
+    return render(request, 'dashboard/all_blogs.html', {
+        "blogs": blogs,
     })
 
 
