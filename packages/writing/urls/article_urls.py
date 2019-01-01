5ddc2bd2c2346be0987e3144e99526278e257c02
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-01-01 17:09:13 +0600

    ques-ans django app

--- packages/writing/urls/article_urls.py
+++ packages/writing/urls/article_urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from ..views import article as views
 
 app_name = 'article'
@@ -6,4 +6,5 @@ app_name = 'article'
 urlpatterns = [
     path('', views.all_articles, name='all_articles'),
     path('section/<slug>/', views.sectionized_article, name='sectionized_article'),
+    path('question-answer/', include('que_ans.urls')),
 ]
