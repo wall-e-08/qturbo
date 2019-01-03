01113d7630988d6e4193a02173ab2f35f1ba70e3
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-01-03 18:56:02 +0600

    shortcode generator

--- packages/distinct_pages/utils/model_dependent.py
+++ packages/distinct_pages/utils/model_dependent.py
@@ -1,3 +1,5 @@
+from django.apps import apps
+from django.conf import settings
 from ..models import Page
 from writing.models import Article, Blog
 
@@ -22,3 +24,25 @@ def get_model_obj_by_slug(slug):
         "model_obj": obj,
         "model_type": model_type,
     }
+
+
+def shortcode_generator(model_as_str: str, id_list: list, **kwargs):
+    """
+    :param model_as_str: 
+    :param id_list: 
+    :param kwargs: 
+    :return: sample return: "[% Page id='1,2,4,5' GG='wp' n1='ok' %]"
+    """
+    model_obj = apps.get_model(app_label='distinct_pages', model_name=model_as_str)
+    id_list2 = id_list.copy()
+    for _id in id_list2:
+        if not model_obj.objects.filter(id=int(_id)).exists():
+            id_list.remove(_id)
+    id_str = ",".join(map(str, id_list))
+    return "{prefix} {obj} id='{ids}' {others} {postfix}".format(
+        prefix=settings.SHORTCODE_PREFIX,
+        obj=model_as_str,
+        ids=id_str,
+        others=' '.join("{}='{}'".format(x[0], x[1]) for x in kwargs.items()),
+        postfix=settings.SHORTCODE_POSTFIX,
+    )
