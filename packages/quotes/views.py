5a8e1f5ff9f1dba87e5dfe37f296a3d87158c868
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-01-10 16:09:06 +0600

    bugfix+cleanup

--- packages/quotes/views.py
+++ packages/quotes/views.py
@@ -89,6 +89,8 @@ def validate_quote_form(request: WSGIRequest) -> JsonResponse:
     :return: Django JsonResponse Object
     """
 
+
+
     print(f" ------------\n| POST DATA  |:\n ------------\n{request.POST}")
 
     form = ApplicantInfoForm(request.POST)
