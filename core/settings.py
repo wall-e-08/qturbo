b5237c813fb9f9025ff57c199a43f96d87579f4b
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-01-13 15:57:35 +0600

    change celery backend url again

--- core/settings.py
+++ core/settings.py
@@ -111,7 +111,7 @@ else:
 ###################
 
 CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
