46e75eaea1e58c6a554dac5a8af606133415ee6d
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-01-27 16:34:40 +0600

    minor change(fixed last commit)

--- packages/quotes/views.py
+++ packages/quotes/views.py
@@ -512,8 +512,8 @@ def stm_plan(request: WSGIRequest, plan_url: str) -> HttpResponse:
         print("Very weird error: {}".format(er))
 
     return render(request,
-                  'quotes/stm_plan.html',
-                  # 'quotes/plans/{0}_info.html'.format(plan["Name"].lower().replace(' ', '_')),
+                  # 'quotes/stm_plan.html',
+                  'quotes/plans/{0}.html'.format(plan["Name"].lower().replace(' ', '_')),
                   {'plan': plan, 'related_plans': related_plans,
                    'quote_request_form_data': quote_request_form_data,
                    'addon_plans': addon_plans, 'selected_addon_plans': selected_addon_plans,
