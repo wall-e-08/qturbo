f08231d8b120844c11e7ffd0941644e521994233
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-02-24 11:54:53 +0600

    fixed error msg logic & min age changed in settings

--- core/settings.py
+++ core/settings.py
@@ -468,7 +468,7 @@ USER_INITIAL_PREFERENCE_DATA = {
 # -----------------------+
 
 USER_PROPERTIES: Dict[str, int] = {
-    'min_age': 21,
+    'min_age': 18,
     'max_age': 99,
 
     'dependents_min_age': 6,
